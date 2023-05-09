45b341597f5076825512a668b851cdc2aecfcfa9
djk3898 <92979584+djk3898@users.noreply.github.com>
2023-05-09 11:53:52 +0200

    Primera versió Diagrama de Classes

---
+++ Zoe/Zoe/Comanda.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zoe
+{
+    public class Comanda
+    {
+        private int numComanda;
+        private DateTime data;
+        private List<Pack> packs;
+        private Usuari comprador;
+        private double total;
+
+        public Pack Pack
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Usuari Usuari
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Comanda(DateTime d, List<Pack> p, Usuari u)
+        {
+            data = d;
+            packs = p;
+            comprador = u;
+            numComanda = GenerarNumComanda();
+        }
+
+        private int GenerarNumComanda()
+        {
+            return Convert.ToInt32(data) + comprador.Id;
+        }
+        public string GenerarFactura()
+        {
+            return "";
+        }
+        public string ToString() { return ""; }
+    }
+}
