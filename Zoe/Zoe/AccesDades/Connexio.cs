8b6e94ec4cfacf43bfafd5fd8157077f3ad9a297
djk3898 <92979584+djk3898@users.noreply.github.com>
2023-05-11 11:58:42 +0200

    SQLConnection

---
+++ Zoe/Zoe/AccesDades/Connexio.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Zoe.AccesDades
+{
+    public class Connexio
+    {
+        //Atributs
+        MySqlConnection connexio;
+        static string servidor = "";
+        static string port = "";
+        static string usuari = "";
+        static string contra = "";
+        static string baseDades = "";
+
+        string stringConnexio = $"server={servidor};port={port};user={usuari};password={contra};database={baseDades};";
+
+        public MySqlConnection Connectar()
+        {
+            try
+            {
+                connexio = new(stringConnexio);
+                connexio.Open();
+
+            }
+            catch(Exception dbConnectionFail) {
+                MessageBox.Show("ERROR\nNo s'ha pogut obrir la connexió amb la BBDD");
+            }
+            finally
+            {
+                if(connexio != null)
+                {
+                    connexio.Close();
+                }
+            }
+        }
+    }
+}
