e3e5be25fb8e42cf50a5970ad1ef9f56d56a8e15
Ls-cow <124570503+Ls-cow@users.noreply.github.com>
2023-05-17 12:34:51 +0200

    Panel de control

---
+++ Zoe/Zoe/Vistas/AdminVista.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Zoe.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para AdminVista.xaml
+    /// </summary>
+    public partial class AdminVista : Window
+    {
+        public AdminVista()
+        {
+            InitializeComponent();
+        }
+    }
+}
